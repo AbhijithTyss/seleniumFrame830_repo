0e16a7a762e709bd68c9f4d3622c66f7a20c97bd
AbhijithTyss <abhijith.styss@gmail.com>
2024-07-11 10:28:18 +0530

    Modified SameeraClass in Framework

--- SeleniumFramework-8-30/src/test/java/sameera/SameeraClass.java
+++ SeleniumFramework-8-30/src/test/java/sameera/SameeraClass.java
@@ -4,6 +4,7 @@ public class SameeraClass {
 
 	public static void main(String[] args) {
 		System.out.println("Added class in Framework");
+		System.out.println("Modified SameeraClass in Framework");
 	}
 
 }
